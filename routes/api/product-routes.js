8f787b41c6473c9409978974d7cdf1bb7d9947da
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-18 23:34:47 -0400

    added function to find products by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,12 +8,10 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.params.id,{
-      include: [{model: Category}, {model: Tag}],
-    });
+    const productData = await Product.findAll();
     res.status(200).json(productData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
 });
 
@@ -21,7 +19,14 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-
+  try {
+    const productData = await Product.findByPk(req.params.id,{
+      include: [{model: Category}, {model: Tag}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
