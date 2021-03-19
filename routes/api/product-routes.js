f1dc24e4e8aa3f5b4579db8f329ca9f24f211c99
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-18 23:37:55 -0400

    added create new product function

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -30,7 +30,14 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
