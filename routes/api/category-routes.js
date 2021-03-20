638a1541166fa58854775020a54e4a26d017d70d
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-20 16:40:22 -0400

    removed array restraints to received data

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-    if (!categoryData[0]) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No category with this id!' });
       return;
     }
@@ -63,7 +63,7 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-    if (!categoryData[0]) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No category with this id!' });
       return;
     }
